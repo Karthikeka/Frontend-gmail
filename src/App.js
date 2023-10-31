af0afac4753f77cd6d943fe60d113375de65f92c
karthikeka <karthikeka94@gmail.com>
2023-10-31 14:09:52 +0530

    password

--- src/App.js
+++ src/App.js
@@ -1,37 +1,29 @@
-import "./App.css";
-
-import {
-  Navigate,
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import { routes } from "./Data/Logo/routes";
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route
-        path={routes.emails.path}
-        element={<Navigate to={`${routes.emails.path}/inbox`} />}
-      />
-      <Route
-        path={routes.invalid.path}
-        element={<Navigate to={`${routes.emails.path}/inbox`} />}
-      />
-      <Route path={routes.main.path} element={<routes.main.element />}>
-        <Route path={routes.view.path} element={<routes.view.element />} />
-        <Route
-          path={`${routes.emails.path}/:type`}
-          element={<routes.emails.element />}
-        />
-      </Route>
-    </Route>
-  )
-);
-
+import Header from "./Components/Header";
+import Login from "./Components/Login";
+import Dashboard from "./Components/Dasboard";
+import Error from "./Components/ErrorFile";
+import {Routes,Route} from "react-router-dom";
+import PasswordReset from "./Components/PasswordReset";
+import ForgotPassword from "./Components/ForgotPassword";
+import Register from "./Components/Register"
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Header/>
+
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/dash" element={<Dashboard/>}/>
+        <Route path="*" element={<Error/>}/>
+        <Route path="/password-reset" element={<PasswordReset/>}/>
+        <Route path="/forgotpassword/:id/:token" element={<ForgotPassword/>}/>
+      </Routes>
+
+    </>
+  );
+
+
 }
 
 export default App;
